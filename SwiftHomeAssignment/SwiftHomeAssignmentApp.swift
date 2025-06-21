ae808c2de81856b5271864a5924b66dce1c9bd21
Dor Luzgarten <dorluzgarten@gmail.com>
2025-06-21 14:43:05 +0300

    Initial Commit

---
+++ SwiftHomeAssignment/SwiftHomeAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftHomeAssignmentApp.swift
+//  SwiftHomeAssignment
+//
+//  Created by Dor Luzgarten on 21/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftHomeAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftHomeAssignmentTests/SwiftHomeAssignmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftHomeAssignmentTests.swift
+//  SwiftHomeAssignmentTests
+//
+//  Created by Dor Luzgarten on 21/06/2025.
+//
+
+import Testing
+@testable import SwiftHomeAssignment
+
+struct SwiftHomeAssignmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
